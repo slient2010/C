0b3e1bd555c3b345bb16c5068668cab1b29c80db
slient2010 <slient2010@163.com>
2020-02-17 18:05:02 +0800

    improvement: fix some syntax errors
    fix some syntax errors.

--- 1基本技能开发技巧/20.c
+++ 1基本技能开发技巧/20.c
@@ -12,21 +12,22 @@ int main(int argc, char *argv[])
 	    {
 	      i=100*x+10*y+z+100*y+10*z+z;
 	      if(i==532)
-		printf ("x=%d,y=%d,z=%d\n",x,y,z);
+		printf("x=%d,y=%d,z=%d\n",x,y,z);
 	    }
   return 0;
 }
 */
+
 /****************************************
 技巧12：统计各种字符个数
 ****************************************/
- /*
+/*
 #include <stdio.h>
 int main(int argc, char *argv[])
 {
   char c;
   int letter=0,digit=0,space=0,other=0;
-  printf ("please input some characters\n");
+  printf("please input some characters\n");
   //当输入的不是回车时执行while循环体
   while((c=getchar())!='\n')
     {
@@ -39,14 +40,16 @@ int main(int argc, char *argv[])
       else
 	other++;
     }
-  printf ("letter=%d,digit=%d,space=%d,other=%d\n",letter,digit,space,other);
+  printf("letter=%d,digit=%d,space=%d,other=%d\n",letter,digit,space,other);
   return 0;
 }
- */
+*/
+
 /****************************************
 技巧13：猴子吃桃
 ****************************************/
-  /*
+
+/*
 #include <stdio.h>
 int main(int argc, char *argv[])
 {
@@ -59,14 +62,15 @@ int main(int argc, char *argv[])
       x2=x1;
       day--;
     }
-  printf ("the total is %d\n",x1);
+  printf("the total is %d\n",x1);
   return 0;
 }
-  */
+*/
+
 /****************************************
 技巧14：删除字符串中连续字符
 ****************************************/
-   /*
+/*
 #include <stdio.h>
 char del(char s[],int pos,int len)   //自定义删除函数，这里采用覆盖方法
  {
@@ -80,17 +84,18 @@ int main(int argc, char *argv[])
 {
   char str[50];
   int position,length;
-  printf ("please input string:\n");
+  printf("please input string:\n");
   gets(str);   //使用gets函数获得字符串
-  printf ("please input delete position:");
+  printf("please input delete position:");
   scanf("%d",&position);
-  printf ("please input delete length:");
+  printf("please input delete length:");
   scanf("%d",&length);
   del(str,position,length);
-  printf ("the final string:%s\n",str);
+  printf("the final string:%s\n",str);
   return 0;
 }
-   */
+*/
+
 /****************************************
 技巧15：求同学的平均身高
 ****************************************/
@@ -110,19 +115,20 @@ int main(int argc, char *argv[])
   float average(float array[],int n);
   float height[100],aver;
   int i,n;
-  printf ("please input the number of students:\n");
+  printf("please input the number of students:\n");
   scanf("%d",&n);
-  printf ("please input student's height:\n");
+  printf("please input student's height:\n");
   for(i=0;i<n;i++)
     scanf("%f",&height[i]);
-  printf ("\n");
+  printf("\n");
   aver=average(height,n);
-  printf ("average height is %6.2f\n",aver);
+  printf("average height is %6.2f\n",aver);
   return 0;
 }
 */
+
 /****************************************
-技巧16：灯塔数量（利用了穷举发）
+技巧16：灯塔数量（利用了穷举法）
 ****************************************/
 /*
 #include <stdio.h>
@@ -130,23 +136,18 @@ int main(int argc, char *argv[])
 {
   int n=1,m,sum,i;
   while(1)
-    {
-      m=n;
-      sum=0;
-      for (i=1; i<8; i++)
-{
-  m=m*2;
-  sum+=m;
- }
+  {
+      m=n; sum=0;
+      for (i=1; i<8; i++) { m=m*2; sum+=m; }
       sum+=n;
       if (sum==765)
-{
-  printf ("the first floor has %d\n",n);
-  printf ("the eight floor has %d\n",m);
-  break;
- }
+      {
+        printf("the first floor has %d\n",n);
+        printf("the eight floor has %d\n",m);
+	break;
+      }
       n++;
-    }
+  }
   return 0;
 }
 */
@@ -165,44 +166,44 @@ int main(int argc, char *argv[])
         sum2=(9*n+171)/100.0;       //第两个学生得到的糖果份数  
         if(sum1 != (int)sum1) continue;       //得到的糖果份数必须为整数  
         if(sum2 != (int)sum2) continue;  
-        if(sum1 == sum2) break;                //只有当sum1与sum2相等的时候，才能穷举出符合条件的结果  
+        if(sum1 == sum2) break; //只有当sum1与sum2相等时，才能穷举出符合条件的结果  
     }  
-    printf("一共有%d个学生\n将糖果分成了%d份\n",(int)(n/sum1),n);       //(n/sum1)即为学生个数  
+    printf("共有%d个学生\n将糖果分成了%d份\n",(int)(n/sum1),n);//(n/sum1)即为学生个数  
 }  
 */
+
 /****************************************
 技巧18：小球下落问题
 ****************************************/
- /*
+/*
 #include <stdio.h>
 int main(int argc, char *argv[])
 {
   float i,h=100,s=100;
   for (i=1; i<=9; i++)
-    {
-      h=h/2;
-      s+=h*2;
-    }
-  printf ("the total length is %f\n",s);
-  printf ("the lenght of tenth time is %f\n",h/2);
+    { h=h/2; s+=h*2; }
+  printf("the total length is %f\n",s);
+  printf("the lenght of tenth time is %f\n",h/2);
   return 0;
 }
- */
+*/
+
 /****************************************
 技巧19:彩球问题
 ****************************************/
-
+/*
 #include <stdio.h>
 int main(int argc, char *argv[])
 {
   int i,j,count;
   puts("the result is:\n");   //向屏幕上输出提示信息
-  printf ("time  red ball  white ball  black ball\n");
+  printf("time  red ball  white ball  black ball\n");
   count = 1;
   for(i=0;i<=3;i++)
     for(j=0;j<=3;j++)
       if((8-i-j)<=6)
-	printf ("%3d%7d%12d%12d\n",count++,i,j,8-i-j);
+	printf("%3d%7d%12d%12d\n",count++,i,j,8-i-j);
   return 0;
 }
+*/
 
